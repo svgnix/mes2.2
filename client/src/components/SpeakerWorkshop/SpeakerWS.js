2422e517c9b67957c2b26138b69d46d6e3aa6809
VanshNyati <vansh.nyati@learner.manipal.edu>
2023-02-12 11:11:20 +0530

    Responsiveness Updated

--- client/src/components/SpeakerWorkshop/SpeakerWS.js
+++ client/src/components/SpeakerWorkshop/SpeakerWS.js
@@ -3,242 +3,242 @@ import "./SpeakerWS.css";
 
 const SpeakerWS = () => {
   return (
-    // <>
-    // <div className="speaker">
-    //   <div className="speaker-details">
-    //   <div className="speaker-img">
-    //     <img
-    //           src="https://i.ibb.co/hYQcVfv/image.png"
-    //       alt="Speaker 1"
-    //       className="circle-img"
-    //     />
-    //   </div>
-    //       <a className="linkedin" href="https://www.linkedin.com/in/vikashkrgupta" target={"_blank"} rel="noreferrer">Vikas Gupta</a>
-    //   </div>
-    //   <div className="speaker-details">
-    //   <div className="speaker-img">
-    //     <img
-    //           src="https://i.ibb.co/R2dtSX8/image.png"
-    //       alt="Speaker 1"
-    //       className="circle-img"
-    //     />
-    //   </div>
-    //       <a className="linkedin" href="https://www.linkedin.com/in/snparvatikar/" target={"_blank"} rel="noreferrer">Sudhinder <br></br>Parvatikar</a>
-    //   </div>
-    //   <div className="speaker-details">
-    //   <div className="speaker-img">
-    //     <img
-    //           src="https://i.ibb.co/3f0TGDF/image.png"
-    //       alt="Speaker 1"
-    //       className="circle-img"
-    //     />
-    //   </div>
-    //       <a className="linkedin" href="https://www.linkedin.com/in/arsakar/" target={"_blank"} rel="noreferrer">Arvind Sasikumar</a>
-    //   </div>
-    //   <div className="speaker-details">
-    //   <div className="speaker-img">
-    //     <img
-    //           src="https://i.ibb.co/2sgNBZD/image.png"
-    //       alt="Speaker 1"
-    //       className="circle-img"
-    //     />
-    //   </div>
-    //       <a className="linkedin" href="https://www.linkedin.com/in/lokeshvenkataswamy/" target={"_blank"} rel="noreferrer">Lokesh <br></br>Venkataswamy</a>
-    //   </div>
-    //   <div className="speaker-details">
-    //   <div className="speaker-img">
-    //     <img
-    //           src="https://i.ibb.co/x1HHrCW/image.png"
-    //       alt="Speaker 1"
-    //       className="circle-img"
-    //     />
-    //   </div>
-    //       <a className="linkedin" href="https://www.linkedin.com/in/tteapen/" target={"_blank"} rel="noreferrer">Dr, Tojin T. Eapen</a>
-    //   </div>
-    //   <div className="speaker-details">
-    //   <div className="speaker-img">
-    //     <img
-    //           src="https://i.ibb.co/ncjpYct/image.png"
-    //       alt="Speaker 1"
-    //       className="circle-img"
-    //     />
-    //   </div>
-    //       <a className="linkedin" href="https://www.linkedin.com/in/bharadwajjyoti/" target={"_blank"} rel="noreferrer">Jyoti Bharadwaj</a>
-    //   </div>
-    // </div>
+    <>
+    <div className="speaker">
+      <div className="speaker-details">
+      <div className="speaker-img">
+        <img
+              src="https://i.ibb.co/hYQcVfv/image.png"
+          alt="Speaker 1"
+          className="circle-img"
+        />
+      </div>
+          <a className="linkedin" href="https://www.linkedin.com/in/vikashkrgupta" target={"_blank"} rel="noreferrer">Vikas Gupta</a>
+      </div>
+      <div className="speaker-details">
+      <div className="speaker-img">
+        <img
+              src="https://i.ibb.co/R2dtSX8/image.png"
+          alt="Speaker 1"
+          className="circle-img"
+        />
+      </div>
+          <a className="linkedin" href="https://www.linkedin.com/in/snparvatikar/" target={"_blank"} rel="noreferrer">Sudhinder <br></br>Parvatikar</a>
+      </div>
+      <div className="speaker-details">
+      <div className="speaker-img">
+        <img
+              src="https://i.ibb.co/3f0TGDF/image.png"
+          alt="Speaker 1"
+          className="circle-img"
+        />
+      </div>
+          <a className="linkedin" href="https://www.linkedin.com/in/arsakar/" target={"_blank"} rel="noreferrer">Arvind Sasikumar</a>
+      </div>
+      <div className="speaker-details">
+      <div className="speaker-img">
+        <img
+              src="https://i.ibb.co/2sgNBZD/image.png"
+          alt="Speaker 1"
+          className="circle-img"
+        />
+      </div>
+          <a className="linkedin" href="https://www.linkedin.com/in/lokeshvenkataswamy/" target={"_blank"} rel="noreferrer">Lokesh <br></br>Venkataswamy</a>
+      </div>
+      <div className="speaker-details">
+      <div className="speaker-img">
+        <img
+              src="https://i.ibb.co/x1HHrCW/image.png"
+          alt="Speaker 1"
+          className="circle-img"
+        />
+      </div>
+          <a className="linkedin" href="https://www.linkedin.com/in/tteapen/" target={"_blank"} rel="noreferrer">Dr, Tojin T. Eapen</a>
+      </div>
+      <div className="speaker-details">
+      <div className="speaker-img">
+        <img
+              src="https://i.ibb.co/ncjpYct/image.png"
+          alt="Speaker 1"
+          className="circle-img"
+        />
+      </div>
+          <a className="linkedin" href="https://www.linkedin.com/in/bharadwajjyoti/" target={"_blank"} rel="noreferrer">Jyoti Bharadwaj</a>
+      </div>
+    </div>
     
-    //   <div className="speaker">
+      <div className="speaker">
         
-    //     <div className="speaker-details">
-    //       <div className="speaker-img">
-    //         <img
-    //           src="https://i.ibb.co/wdDWxQJ/image.png"
-    //           alt="Speaker 1"
-    //           className="circle-img"
-    //         />
-    //       </div>
-    //       <a className="linkedin" href="https://www.linkedin.com/in/akshayjohri/" target={"_blank"} rel="noreferrer">Akshay Johri</a>
-    //     </div>
-    //     <div className="speaker-details">
-    //       <div className="speaker-img">
-    //         <img
-    //           src="https://i.ibb.co/71gWyv2/image.png"
-    //           alt="Speaker 1"
-    //           className="circle-img"
-    //         />
-    //       </div>
-    //       <a className="linkedin" href="https://www.linkedin.com/in/riyajn/" target={"_blank"} rel="noreferrer">Riya Jain</a>
-    //     </div>
-    //     <div className="speaker-details">
-    //       <div className="speaker-img">
-    //         <img
-    //           src="https://i.ibb.co/VDqqq02/image.png"
-    //           alt="Speaker 1"
-    //           className="circle-img"
-    //         />
-    //       </div>
-    //       <a className="linkedin" href="https://www.linkedin.com/in/saxenashobhit/" target={"_blank"} rel="noreferrer">Shobhit Saxena</a>
+        <div className="speaker-details">
+          <div className="speaker-img">
+            <img
+              src="https://i.ibb.co/wdDWxQJ/image.png"
+              alt="Speaker 1"
+              className="circle-img"
+            />
+          </div>
+          <a className="linkedin" href="https://www.linkedin.com/in/akshayjohri/" target={"_blank"} rel="noreferrer">Akshay Johri</a>
+        </div>
+        <div className="speaker-details">
+          <div className="speaker-img">
+            <img
+              src="https://i.ibb.co/71gWyv2/image.png"
+              alt="Speaker 1"
+              className="circle-img"
+            />
+          </div>
+          <a className="linkedin" href="https://www.linkedin.com/in/riyajn/" target={"_blank"} rel="noreferrer">Riya Jain</a>
+        </div>
+        <div className="speaker-details">
+          <div className="speaker-img">
+            <img
+              src="https://i.ibb.co/VDqqq02/image.png"
+              alt="Speaker 1"
+              className="circle-img"
+            />
+          </div>
+          <a className="linkedin" href="https://www.linkedin.com/in/saxenashobhit/" target={"_blank"} rel="noreferrer">Shobhit Saxena</a>
         
-    //     </div>
-    //     <div className="speaker-details">
-    //       <div className="speaker-img">
-    //         <img
-    //           src="https://i.ibb.co/dP95J8P/image.png"
-    //           alt="Speaker 1"
-    //           className="circle-img"
-    //         />
-    //       </div>
-    //       <a className="linkedin" href="https://www.linkedin.com/in/vinodaravindakshan/" target={"_blank"} rel="noreferrer">Vinod <br></br>Aravindakshan</a>
-    //     </div>
-    //   </div>
-    // </>
-      <>
-        <div className="speaker">
-          <div class="speaker-detail">
-            <div class="speaker-img">
-              <img
-                src="https://i.ibb.co/hYQcVfv/image.png"
-                alt="user"
-              />
-            </div>
-            <div class="speaker-info">
-            <a className="linkedin" href="https://www.linkedin.com/in/vikashkrgupta" target={"_blank"} rel="noreferrer">Vikas Gupta</a>
-            </div>
-          </div>
-    
-          <div class="speaker-detail">
-            <div class="speaker-img">
-              <img
-                src="https://png.pngtree.com/png-vector/20210604/ourmid/pngtree-gray-avatar-placeholder-png-image_3416697.jpg"
-                alt="user"
-              />
-            </div>
-            <div class="speaker-info">
-              <a href="#!">Lorem Ipsum</a>
-            </div>
-          </div>
-    
-          <div class="speaker-detail">
-            <div class="speaker-img">
-              <img
-                src="https://png.pngtree.com/png-vector/20210604/ourmid/pngtree-gray-avatar-placeholder-png-image_3416697.jpg"
-                alt="user"
-              />
-            </div>
-            <div class="speaker-info">
-              <a href="#!">Lorem Ipsum</a>
-            </div>
-          </div>
-    
-          <div class="speaker-detail">
-            <div class="speaker-img">
-              <img
-                src="https://png.pngtree.com/png-vector/20210604/ourmid/pngtree-gray-avatar-placeholder-png-image_3416697.jpg"
-                alt="user"
-              />
-            </div>
-            <div class="speaker-info">
-              <a href="#!">Lorem Ipsum</a>
-            </div>
-          </div>
-    
-          <div class="speaker-detail">
-            <div class="speaker-img">
-              <img
-                src="https://png.pngtree.com/png-vector/20210604/ourmid/pngtree-gray-avatar-placeholder-png-image_3416697.jpg"
-                alt="user"
-              />
-            </div>
-            <div class="speaker-info">
-              <a href="#!">Lorem Ipsum</a>
-            </div>
-          </div>
         </div>
-
-
-        <div className="speaker">
-          <div class="speaker-detail">
-            <div class="speaker-img">
-              <img
-                src="https://png.pngtree.com/png-vector/20210604/ourmid/pngtree-gray-avatar-placeholder-png-image_3416697.jpg"
-                alt="user"
-              />
-            </div>
-            <div class="speaker-info">
-              <a href="#!">Lorem Ipsum</a>
-            </div>
+        <div className="speaker-details">
+          <div className="speaker-img">
+            <img
+              src="https://i.ibb.co/dP95J8P/image.png"
+              alt="Speaker 1"
+              className="circle-img"
+            />
           </div>
-    
-          <div class="speaker-detail">
-            <div class="speaker-img">
-              <img
-                src="https://png.pngtree.com/png-vector/20210604/ourmid/pngtree-gray-avatar-placeholder-png-image_3416697.jpg"
-                alt="user"
-              />
-            </div>
-            <div class="speaker-info">
-              <a href="#!">Lorem Ipsum</a>
-            </div>
-          </div>
-    
-          <div class="speaker-detail">
-            <div class="speaker-img">
-              <img
-                src="https://png.pngtree.com/png-vector/20210604/ourmid/pngtree-gray-avatar-placeholder-png-image_3416697.jpg"
-                alt="user"
-              />
-            </div>
-            <div class="speaker-info">
-              <a href="#!">Lorem Ipsum</a>
-            </div>
-          </div>
-    
-          <div class="speaker-detail">
-            <div class="speaker-img">
-              <img
-                src="https://png.pngtree.com/png-vector/20210604/ourmid/pngtree-gray-avatar-placeholder-png-image_3416697.jpg"
-                alt="user"
-              />
-            </div>
-            <div class="speaker-info">
-              <a href="#!">Lorem Ipsum</a>
-            </div>
-          </div>
-    
-          <div class="speaker-detail">
-            <div class="speaker-img">
-              <img
-                src="https://png.pngtree.com/png-vector/20210604/ourmid/pngtree-gray-avatar-placeholder-png-image_3416697.jpg"
-                alt="user"
-              />
-            </div>
-            <div class="speaker-info">
-              <a href="#!">Lorem Ipsum</a>
-            </div>
-          </div>
+          <a className="linkedin" href="https://www.linkedin.com/in/vinodaravindakshan/" target={"_blank"} rel="noreferrer">Vinod <br></br>Aravindakshan</a>
         </div>
+      </div>
+    </>
+      // <>
+      //   <div className="speaker">
+      //     <div class="speaker-detail">
+      //       <div class="speaker-img">
+      //         <img
+      //           src="https://i.ibb.co/hYQcVfv/image.png"
+      //           alt="user"
+      //         />
+      //       </div>
+      //       <div class="speaker-info">
+      //       <a className="linkedin" href="https://www.linkedin.com/in/vikashkrgupta" target={"_blank"} rel="noreferrer">Vikas Gupta</a>
+      //       </div>
+      //     </div>
+    
+      //     <div class="speaker-detail">
+      //       <div class="speaker-img">
+      //         <img
+      //           src="https://png.pngtree.com/png-vector/20210604/ourmid/pngtree-gray-avatar-placeholder-png-image_3416697.jpg"
+      //           alt="user"
+      //         />
+      //       </div>
+      //       <div class="speaker-info">
+      //         <a href="#!">Lorem Ipsum</a>
+      //       </div>
+      //     </div>
+    
+      //     <div class="speaker-detail">
+      //       <div class="speaker-img">
+      //         <img
+      //           src="https://png.pngtree.com/png-vector/20210604/ourmid/pngtree-gray-avatar-placeholder-png-image_3416697.jpg"
+      //           alt="user"
+      //         />
+      //       </div>
+      //       <div class="speaker-info">
+      //         <a href="#!">Lorem Ipsum</a>
+      //       </div>
+      //     </div>
+    
+      //     <div class="speaker-detail">
+      //       <div class="speaker-img">
+      //         <img
+      //           src="https://png.pngtree.com/png-vector/20210604/ourmid/pngtree-gray-avatar-placeholder-png-image_3416697.jpg"
+      //           alt="user"
+      //         />
+      //       </div>
+      //       <div class="speaker-info">
+      //         <a href="#!">Lorem Ipsum</a>
+      //       </div>
+      //     </div>
+    
+      //     <div class="speaker-detail">
+      //       <div class="speaker-img">
+      //         <img
+      //           src="https://png.pngtree.com/png-vector/20210604/ourmid/pngtree-gray-avatar-placeholder-png-image_3416697.jpg"
+      //           alt="user"
+      //         />
+      //       </div>
+      //       <div class="speaker-info">
+      //         <a href="#!">Lorem Ipsum</a>
+      //       </div>
+      //     </div>
+      //   </div>
 
-        </>
+
+      //   <div className="speaker">
+      //     <div class="speaker-detail">
+      //       <div class="speaker-img">
+      //         <img
+      //           src="https://png.pngtree.com/png-vector/20210604/ourmid/pngtree-gray-avatar-placeholder-png-image_3416697.jpg"
+      //           alt="user"
+      //         />
+      //       </div>
+      //       <div class="speaker-info">
+      //         <a href="#!">Lorem Ipsum</a>
+      //       </div>
+      //     </div>
+    
+      //     <div class="speaker-detail">
+      //       <div class="speaker-img">
+      //         <img
+      //           src="https://png.pngtree.com/png-vector/20210604/ourmid/pngtree-gray-avatar-placeholder-png-image_3416697.jpg"
+      //           alt="user"
+      //         />
+      //       </div>
+      //       <div class="speaker-info">
+      //         <a href="#!">Lorem Ipsum</a>
+      //       </div>
+      //     </div>
+    
+      //     <div class="speaker-detail">
+      //       <div class="speaker-img">
+      //         <img
+      //           src="https://png.pngtree.com/png-vector/20210604/ourmid/pngtree-gray-avatar-placeholder-png-image_3416697.jpg"
+      //           alt="user"
+      //         />
+      //       </div>
+      //       <div class="speaker-info">
+      //         <a href="#!">Lorem Ipsum</a>
+      //       </div>
+      //     </div>
+    
+      //     <div class="speaker-detail">
+      //       <div class="speaker-img">
+      //         <img
+      //           src="https://png.pngtree.com/png-vector/20210604/ourmid/pngtree-gray-avatar-placeholder-png-image_3416697.jpg"
+      //           alt="user"
+      //         />
+      //       </div>
+      //       <div class="speaker-info">
+      //         <a href="#!">Lorem Ipsum</a>
+      //       </div>
+      //     </div>
+    
+      //     <div class="speaker-detail">
+      //       <div class="speaker-img">
+      //         <img
+      //           src="https://png.pngtree.com/png-vector/20210604/ourmid/pngtree-gray-avatar-placeholder-png-image_3416697.jpg"
+      //           alt="user"
+      //         />
+      //       </div>
+      //       <div class="speaker-info">
+      //         <a href="#!">Lorem Ipsum</a>
+      //       </div>
+      //     </div>
+      //   </div>
+
+      //   </>
       );
     };
   
